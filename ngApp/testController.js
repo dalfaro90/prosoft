29d387e79dde96caec12cbd6e66968bf8ee4c372
Danny Alfaro <dannyalfaro90@hotmail.com>
2017-08-12 13:11:43 -0600

    Add fields (numberOfDays, countryCode, startDate) to page.

--- ngApp/testController.js
+++ ngApp/testController.js
@@ -3,6 +3,27 @@
 
     angular.module('prosoft.test').controller('testController', function (holidayFactory) {
         var vm = this;
+
+        vm.openCalendar = openCalendar;
+
+        function init() {
+            vm.model = getDefaultValues();
+        }
+
+        function openCalendar() {
+            vm.openedCalendar = true;
+        }
+
+        function getDefaultValues(){
+            return {
+                numberOfDays: 0,
+                countryCode: 'US',
+                startDate: new Date()
+            };
+        }
+        
+        init();
+
     });
 
 })();
